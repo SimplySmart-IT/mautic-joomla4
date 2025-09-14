ac763a318a26d0fae8fefaa959b2a092f7335eed
Martina Scholz <martina@simplysmart-it.de>
2025-09-14 13:30:54 +0200

    fix: add missing DatabaseAwareTrait use statement

--- plg_system_mautic/src/Extension/Mautic.php
+++ plg_system_mautic/src/Extension/Mautic.php
@@ -30,6 +30,7 @@ use Joomla\Utilities\ArrayHelper;
 use Mautic\Plugin\System\Mautic\Helper\MauticApiHelper;
 use Joomla\Event\SubscriberInterface;
 use Joomla\CMS\Event\Model;
+use Joomla\Database\DatabaseAwareTrait;
 use Mautic\Plugin\System\Mautic\Features\OAuth2Client;
 use Mautic\Plugin\System\Mautic\Features\PrepareContentTrait;
 use Mautic\Plugin\System\Mautic\Features\TokenTrait;
